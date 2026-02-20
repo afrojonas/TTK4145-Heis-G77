2088019c951a84d8830f9633e3f84c0987b6b01f
afrojonas <jonas.froiland@gmail.com>
2026-02-20 11:09:36 +0100

    added config

--- heis/utdelt/Driver-go/config/config.go
+++ heis/utdelt/Driver-go/config/config.go
@@ -1,16 +1,17 @@
 package config
 
+//Simple config for elevators
 import (
 	"time"
 )
 
 const (
-	NumFloors        = 4
-	NumElevators     = 3
-	NumButtons       = 3
-	PeersPortNumber  = 58735
-	BcastPortNumber  = 58750
-	Buffer		     = 1024
+	NumFloors       = 4
+	NumElevators    = 3
+	NumButtons      = 3
+	PeersPortNumber = 58735
+	BcastPortNumber = 58750
+	Buffer          = 1024
 
 	DisconnectTime   = 1 * time.Second
 	DoorOpenDuration = 3 * time.Second
