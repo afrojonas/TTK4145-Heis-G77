5c81e737050830a5c7d55aff85ca67e88a005335
jacob-andreas-reistad <reistad@gmail.com>
2026-03-10 08:07:11 +0100

    no changes

--- heis/main.go
+++ heis/main.go
@@ -38,9 +38,9 @@ func main() {
 // 	elevio.SetMotorDirection(d)
 
 // 	drv_buttons := make(chan elevio.ButtonEvent)
-// 	drv_floors := make(chan int)
-// 	drv_obstr := make(chan bool)
-// 	drv_stop := make(chan bool)
+// 	drv_floors  := make(chan int)
+// 	drv_obstr   := make(chan bool)
+// 	drv_stop    := make(chan bool)
 
 // 	go elevio.PollButtons(drv_buttons)
 // 	go elevio.PollFloorSensor(drv_floors)
